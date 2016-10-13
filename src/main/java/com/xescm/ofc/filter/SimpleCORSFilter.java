69c48cf5bdcae7acaaf1de1f14764cb3dd59451a
王松涛 <wangst@dingtalk.com>
2016-10-13 11:13:26 +0800

    修改CSS

--- src/main/java/com/xescm/ofc/filter/SimpleCORSFilter.java
+++ src/main/java/com/xescm/ofc/filter/SimpleCORSFilter.java
@@ -16,7 +16,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Authorization");
-		response.setHeader("Access-Control-Expose-Headers", "newtoken");
+//		response.setHeader("Access-Control-Expose-Headers", "newtoken");
 		chain.doFilter(req, res);
 	}
 
