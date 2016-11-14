f1d706bb76a8f2346fbb20aa4501b7c7105160f4
yangdongxu <1037735036@qq.com>
2016-11-14 15:02:01 +0800

    4444

---
+++ src/main/java/com/xescm/ofc/mq/Consumer/DefaultMqConsumer.java
@@ -0,0 +1,49 @@
+package com.xescm.ofc.mq.Consumer;
+
+import com.aliyun.openservices.ons.api.Consumer;
+import com.aliyun.openservices.ons.api.ONSFactory;
+import com.aliyun.openservices.ons.api.PropertyKeyConst;
+import com.xescm.ofc.config.MqConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Properties;
+
+/**
+ * Created by MT on 2016/11/11.
+ */
+@Component
+public class DefaultMqConsumer  {
+    Logger logger = LoggerFactory.getLogger(DefaultMqConsumer.class);
+
+    @Resource
+    MqConfig mqConfig;
+
+    public void MqConsumer() {
+        logger.info("XX消费开始---:");
+        Consumer consumer = ONSFactory.createConsumer(consumerProperties());
+        consumer.subscribe(mqConfig.getTopic(), mqConfig.getTag(), new MessageListenerImpl());
+        consumer.start();
+
+        //等待固定时间防止进程退出
+        try {
+            Thread.sleep(200000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private Properties consumerProperties(){
+        Properties consumerProperties = new Properties();
+        consumerProperties.setProperty(PropertyKeyConst.ConsumerId, mqConfig.getConsumerId());
+        consumerProperties.setProperty(PropertyKeyConst.AccessKey, mqConfig.getAccessKey());
+        consumerProperties.setProperty(PropertyKeyConst.SecretKey, mqConfig.getSecretKey());
+        consumerProperties.setProperty(PropertyKeyConst.ONSAddr, "http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet");
+        return consumerProperties;
+    }
+
+
+}
---
+++ src/main/java/com/xescm/ofc/utils/MQUtil.java
@@ -0,0 +1,19 @@
+package com.xescm.ofc.utils;
+
+
+import java.util.Properties;
+
+/**
+ * Created by sql on 2016/11/10.
+ */
+public class MQUtil {
+
+
+    public static void propertiesUtil(Properties tranProducerProperties) {
+
+//        tranProducerProperties.setProperty(PropertyKeyConst.ProducerId, MqConfig.PRODUCER_ID);
+//        tranProducerProperties.setProperty(PropertyKeyConst.AccessKey, MqConfig.ACCESS_KEY);
+//        tranProducerProperties.setProperty(PropertyKeyConst.SecretKey, MqConfig.SECRET_KEY);
+//        tranProducerProperties.setProperty(PropertyKeyConst.ONSAddr, "http://onsaddr-internet.aliyun.com/rocketmq/nsaddr4client-internet");
+    }
+}
