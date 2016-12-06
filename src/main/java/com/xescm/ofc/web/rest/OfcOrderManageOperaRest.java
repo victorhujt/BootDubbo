f60909a013641df701193eb83ba27eac7e8702b1
zhanghaiyang <984293834@qq.com>
2016-12-06 14:47:30 +0800

    修改订单跟踪

--- src/main/java/com/xescm/ofc/web/rest/OfcOrderManageOperaRest.java
+++ src/main/java/com/xescm/ofc/web/rest/OfcOrderManageOperaRest.java
@@ -17,7 +17,6 @@ import com.xescm.uam.utils.wrap.Wrapper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
