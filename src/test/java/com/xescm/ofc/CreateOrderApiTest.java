2aa7fb4fbced2398c3f6fb2e39db48b273680087
罗迎豪 <lyhluo@163.com>
2016-12-09 18:10:09 +0800

    Merge branch 'dev' of devtool.xianyiscm.com:cus/xescm-ofc into dev
    # Conflicts:
    #	src/main/java/com/xescm/ofc/config/MqConfig.java

--- src/test/java/com/xescm/ofc/CreateOrderApiTest.java
+++ src/test/java/com/xescm/ofc/CreateOrderApiTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.xescm.ofc.domain.*;
 import com.xescm.ofc.model.dto.coo.CreateOrderEntity;
 import com.xescm.ofc.model.dto.coo.CreateOrderTrans;
+import com.xescm.ofc.model.dto.dms.DmsTransferRecordDto;
+import com.xescm.ofc.service.OfcDmsCallbackStatusService;
 import com.xescm.ofc.service.OfcOrderManageOperService;
 import com.xescm.ofc.utils.JsonUtil;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -14,6 +16,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,119 +28,22 @@ import java.util.List;
 //@SpringApplicationConfiguration(classes = XescmOfcApplication.class)
 //@WebAppConfiguration
 public class CreateOrderApiTest {
-/*
-    @Autowired
-    private CreateOrderService createOrderService;
 
     @Autowired
-    private OfcCreateOrderService ofcCreateOrderService;
+    private OfcDmsCallbackStatusService ofcDmsCallbackStatusService;
 
     @Test
-    public void testOfcCreateOrderService() {
-        System.out.println(ofcCreateOrderService.queryCountByOrderStatus("SO20161111000006","10"));
+    public void tt(){
+        DmsTransferRecordDto dmsTransferRecordDto = new DmsTransferRecordDto();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        dmsTransferRecordDto.setCreatedTime(sdf.format(new Date()));
+        ofcDmsCallbackStatusService.receiveDmsCallbackStatus(dmsTransferRecordDto);
     }
 
 
-    @Test
-    public void testCreateOrder() {
-        System.out.println("testCreateOrder");
-        String json = "[{\n" +
-                "        \"custOrderCode\": \"D161115107629044\",\n" +
-                "        \"orderTime\": \"2016-11-15\",\n" +
-                "        \"custCode\": null,\n" +
-                "        \"custName\": null,\n" +
-                "        \"orderType\": \"60\",\n" +
-                "        \"businessType\": \"600\",\n" +
-                "        \"notes\": \"订单备注\",\n" +
-                "        \"storeCode\": null,\n" +
-                "        \"orderSource\": \"EDI\",\n" +
-                "        \"expandSaleOrg\": null,\n" +
-                "        \"expandProGroup\": null,\n" +
-                "        \"expandSaleDep\": null,\n" +
-                "        \"expandSaleGroup\": null,\n" +
-                "        \"expandSaleDepDes\": null,\n" +
-                "        \"expandSaleGroupDes\": null,\n" +
-                "        \"quantity\": \"1111\",\n" +
-                "        \"weight\": \"9212.0\",\n" +
-                "        \"cubage\": null,\n" +
-                "        \"totalStandardBox\": null,\n" +
-                "        \"transRequire\": null,\n" +
-                "        \"pickupTime\": null,\n" +
-                "        \"expectedArrivedTime\": null,\n" +
-                "        \"consignorName\": \"鲜易网\",\n" +
-                "        \"consignorContact\": \"鲜易网\",\n" +
-                "        \"consignorPhone\": \"400-662-6366\",\n" +
-                "        \"consignorFax\": null,\n" +
-                "        \"consignorEmail\": null,\n" +
-                "        \"consignorZip\": null,\n" +
-                "        \"consignorProvince\": \"河南\",\n" +
-                "        \"consignorCity\": \"郑州\",\n" +
-                "        \"consignorCounty\": \"郑州新区\",\n" +
-                "        \"consignorTown\": null,\n" +
-                "        \"consignorAddress\": \"东风南路七里河路交叉口绿地之窗云峰座\",\n" +
-                "        \"consigneeName\": \"李歌\",\n" +
-                "        \"consigneeContact\": \"李歌\",\n" +
-                "        \"consigneePhone\": \"18637711063\",\n" +
-                "        \"consigneeFax\": null,\n" +
-                "        \"consigneeEmail\": null,\n" +
-                "        \"consigneeZip\": null,\n" +
-                "        \"consigneeProvince\": \"河南\",\n" +
-                "        \"consigneeCity\": \"南阳市\",\n" +
-                "        \"consigneeCounty\": \"宛城区\",\n" +
-                "        \"consigneeTown\": null,\n" +
-                "        \"consigneeAddress\": \"府衙小吃街\",\n" +
-                "        \"warehouseCode\": null,\n" +
-                "        \"warehouseName\": null,\n" +
-                "        \"provideTransport\": null,\n" +
-                "        \"supportName\": null,\n" +
-                "        \"supportContact\": null,\n" +
-                "        \"supportPhone\": null,\n" +
-                "        \"supportFax\": null,\n" +
-                "        \"supportEmail\": null,\n" +
-                "        \"supportZip\": null,\n" +
-                "        \"supportProvince\": null,\n" +
-                "        \"supportCity\": null,\n" +
-                "        \"supportCounty\": null,\n" +
-                "        \"supportTown\": null,\n" +
-                "        \"supportAddress\": null,\n" +
-                "        \"arriveTime\": null,\n" +
-                "        \"plateNumber\": null,\n" +
-                "        \"driverName\": null,\n" +
-                "        \"contactNumber\": null,\n" +
-                "        \"serviceCharge\": null,\n" +
-                "        \"orderAmount\": \"1932\",\n" +
-                "        \"paymentAmount\": \"1932\",\n" +
-                "        \"collectLoanAmount\": \"0\",\n" +
-                "        \"collectServiceCharge\": null,\n" +
-                "        \"collectFlag\": null,\n" +
-                "        \"printInvoice\": null,\n" +
-                "        \"buyerPaymentMethod\": \"6830\",\n" +
-                "        \"insure\": null,\n" +
-                "        \"insureValue\": null,\n" +
-                "        \"createOrderGoodsInfos\": [\n" +
-                "            {\n" +
-                "                \"goodsCode\": null,\n" +
-                "                \"goodsName\": \"鱿鱼切半40克 4.35kg/箱\",\n" +
-                "                \"goodsSpec\": \"4.35kg/箱\",\n" +
-                "                \"unit\": \"箱\",\n" +
-                "                \"quantity\": \"21\",\n" +
-                "                \"unitPrice\": \"92\",\n" +
-                "                \"productionBatch\": null,\n" +
-                "                \"productionTime\": null,\n" +
-                "                \"invalidTime\": null\n" +
-                "            }\n" +
-                "        ],\n" +
-                "        \"baseId\": \"40551\"\n" +
-                "    }]";
-        try {
-            createOrderService.createOrder(json);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
-
-@Autowired
-private OfcOrderManageOperService ofcOrderManageOperService;
+    @Autowired
+    private OfcOrderManageOperService ofcOrderManageOperService;
 
     @Test
     public void test() {
