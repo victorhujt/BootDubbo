13c014b00b6a165208477be4b977a30f4b872d72
胡金涛 <victorhjt@163.com>
2016-12-14 09:21:41 +0800

    拍照录单单条订单的查询

--- src/main/java/com/xescm/ofc/config/OSSConfigureConfig.java
+++ src/main/java/com/xescm/ofc/config/OSSConfigureConfig.java
@@ -1,7 +1,107 @@
 package com.xescm.ofc.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 /**
- * Created by victor on 2016/12/12.
+ * Created by hujintao on 2016/12/12.
  */
+@Configuration
+@ConfigurationProperties(prefix = OSSConfigureConfig.OSS_CONF)
 public class OSSConfigureConfig {
+
+    public final static String OSS_CONF = "oss_conf";
+
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public void setEndpoint(String endpoint) {
+        this.endpoint = endpoint;
+    }
+
+    public String getOutEndpoint() {
+        return outEndpoint;
+    }
+
+    public void setOutEndpoint(String outEndpoint) {
+        this.outEndpoint = outEndpoint;
+    }
+
+    public String getAccessKeyId() {
+        return accessKeyId;
+    }
+
+    public void setAccessKeyId(String accessKeyId) {
+        this.accessKeyId = accessKeyId;
+    }
+
+    public String getAccessKeySecret() {
+        return accessKeySecret;
+    }
+
+    public void setAccessKeySecret(String accessKeySecret) {
+        this.accessKeySecret = accessKeySecret;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    public int getDelayHour() {
+        return delayHour;
+    }
+
+    public void setDelayHour(int delayHour) {
+        this.delayHour = delayHour;
+    }
+
+    public String getRemotePath() {
+        return remotePath;
+    }
+
+    public void setRemotePath(String remotePath) {
+        this.remotePath = remotePath;
+    }
+
+    /**
+     * OSS 访问域名[内网]
+
+     */
+    private String endpoint;
+
+    /**
+     * OSS 访问域名[外网]
+     */
+    private String outEndpoint;
+    /**
+     * key ID
+     */
+    private String accessKeyId;
+
+    /**
+     * key密钥
+     */
+    private String accessKeySecret;
+
+    /**
+     * 文件服务器根目录
+     */
+    private String bucketName;
+
+    /**
+     * 延迟生效时间 单位:h
+     */
+    private int delayHour;
+
+    /**
+     * 远端路径
+     */
+    private String remotePath;
+
+
 }
--- src/main/java/com/xescm/ofc/service/impl/OfcMobileOrderServiceImpl.java
+++ src/main/java/com/xescm/ofc/service/impl/OfcMobileOrderServiceImpl.java
@@ -5,6 +5,7 @@ import com.xescm.ofc.constant.OrderConstConstant;
 import com.xescm.ofc.domain.OfcMobileOrder;
 import com.xescm.ofc.service.OfcMobileOrderService;
 import com.xescm.ofc.utils.CodeGenUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -25,7 +26,45 @@ public class OfcMobileOrderServiceImpl extends BaseService<OfcMobileOrder>  impl
     private CodeGenUtils codeGenUtils;
     @Override
     public OfcMobileOrder saveOfcMobileOrder(OfcMobileOrder ofcMobileOrder) {
+
+
+
         ofcMobileOrder.setMobileOrderCode(codeGenUtils.getNewWaterCode("SN",6));
+        //上传图片
+        if(!StringUtils.isEmpty(ofcMobileOrder.getImg1Url())){
+
+
+
+
+        }
+
+        if(!StringUtils.isEmpty(ofcMobileOrder.getImg2Url())){
+
+
+
+
+        }
+
+        if(!StringUtils.isEmpty(ofcMobileOrder.getImg3Url())){
+
+
+
+
+        }
+
+        if(!StringUtils.isEmpty(ofcMobileOrder.getImg4Url())){
+
+
+
+
+        }
+
+
+
+
+
+
+
         ofcMobileOrder.setBuniessType(OrderConstConstant.TRANSPORTORDER);
         save(ofcMobileOrder);
         return ofcMobileOrder;
