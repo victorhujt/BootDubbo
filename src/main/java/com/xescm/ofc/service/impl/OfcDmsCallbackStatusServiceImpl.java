3d6d535146fb1971e4c11c08c7de3da553f5aaa1
罗迎豪 <lyhluo@163.com>
2016-12-15 11:19:41 +0800

    删除多引的包和多余的注入

--- src/main/java/com/xescm/ofc/service/impl/OfcDmsCallbackStatusServiceImpl.java
+++ src/main/java/com/xescm/ofc/service/impl/OfcDmsCallbackStatusServiceImpl.java
@@ -15,7 +15,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
