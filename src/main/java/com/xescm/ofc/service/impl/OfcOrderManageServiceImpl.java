a259339a95d30f999b4675d1b6c9e47a9797467b
yangdongxu <1037735036@qq.com>
2016-12-18 17:03:34 +0800

    修理异常

--- src/main/java/com/xescm/ofc/service/impl/OfcOrderManageServiceImpl.java
+++ src/main/java/com/xescm/ofc/service/impl/OfcOrderManageServiceImpl.java
@@ -1843,7 +1843,7 @@ public class OfcOrderManageServiceImpl  implements OfcOrderManageService {
             logger.info("#################################取货不配送,调用区域覆盖接口#######################");
             Wrapper<List<RmcServiceCoverageForOrderVo>> rmcPickupList = feignRMcServiceCoverageAPIClient.queryServiceCoverageListForOrder(rmcServiceCoverageForOrderVo);
             if(rmcPickupList!=null && PubUtils.isNotNullAndBiggerSize(rmcPickupList.getResult(), 0)){
-                logger.info("#####################接口返回数据为：{}###########################",rmcPickupList.getResult().get(0));
+                logger.info("#####################接口返回数据为：{}###########################",rmcPickupList.getResult().get(0).toString());
                 return rmcPickupList.getResult().get(0);
             }else {
                 logger.info("#####################接口返回数据为：{}###########################","");
@@ -1855,7 +1855,7 @@ public class OfcOrderManageServiceImpl  implements OfcOrderManageService {
             logger.info("#################################配送不提货,调用区域覆盖接口#######################");
             Wrapper<List<RmcServiceCoverageForOrderVo>> rmcRecipientList = feignRMcServiceCoverageAPIClient.queryServiceCoverageListForOrder(rmcServiceCoverageForOrderVo);
             if(rmcRecipientList!=null && PubUtils.isNotNullAndBiggerSize(rmcRecipientList.getResult(), 0)){
-                logger.info("#####################接口返回数据为：{}###########################",rmcRecipientList.getResult().get(0));
+                logger.info("#####################接口返回数据为：{}###########################",rmcRecipientList.getResult().get(0).toString());
                 return rmcRecipientList.getResult().get(0);
             }else{
                 logger.info("#####################接口返回数据为：{}###########################","");
