8bf6ad678c78d4af57a5bf19309e1e58f59f9560
罗迎豪 <lyhluo@163.com>
2016-12-29 15:33:27 +0800

    HSF单元测试

--- src/test/java/com/xescm/ofc/CreateOrderApiTest.java
+++ src/test/java/com/xescm/ofc/CreateOrderApiTest.java
@@ -1,14 +1,40 @@
 package com.xescm.ofc;
 
+import com.xescm.ofc.domain.OfcPlanFedBackCondition;
+import com.xescm.ofc.service.OfcPlanFedBackService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Date;
+
 /**
  * Created by hiyond on 2016/11/18.
  */
 
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = XescmOfcApplication.class)
-//@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = XescmOfcApplication.class)
+@WebAppConfiguration
 public class CreateOrderApiTest {
 
+    {
+        HsfXml.loadConsumer();
+    }
+
+    @Autowired
+    private OfcPlanFedBackService ofcPlanFedBackService;
+
+    @Test
+    public void sdf(){
+        OfcPlanFedBackCondition ofcPlanFedBackCondition = new OfcPlanFedBackCondition();
+        ofcPlanFedBackCondition.setTransportNo("TP161229000016");
+        ofcPlanFedBackCondition.setStatus("已签收");
+        ofcPlanFedBackCondition.setTraceTime(new Date());
+        ofcPlanFedBackService.planFedBack(ofcPlanFedBackCondition,"123");
+    }
    // @Autowired
   //  private OfcOssManagerService ofcOssManagerService;
 
