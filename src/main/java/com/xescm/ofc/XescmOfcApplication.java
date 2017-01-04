4c7c13d328d45eded9d66f4418f6a0678fc7a4c6
韩光芳 <hgh.han@gmail.com>
2017-01-04 15:22:38 +0800

    修改EDAS分组读取配置

--- src/main/java/com/xescm/ofc/XescmOfcApplication.java
+++ src/main/java/com/xescm/ofc/XescmOfcApplication.java
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.servlet.MultipartConfigElement;
 
-@ImportResource(locations={"classpath:ofc-hsf-provider-test.xml","classpath:ofc-hsf-consumer-test.xml"})
+@ImportResource(locations={"classpath:edas-hsf-config.xml"})
 @EnableTransactionManagement // 开启注解事务管理，等同于xml配置文件中的 <tx:annotation-driven />
 @EnableAutoConfiguration
 @ComponentScan
