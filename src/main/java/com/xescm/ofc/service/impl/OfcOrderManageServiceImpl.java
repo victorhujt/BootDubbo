2834ccd5b77995f6f70d7d9aacb0d9088e589ba3
胡金涛 <victorhjt@163.com>
2017-01-04 15:15:55 +0800

    去掉多次尝试调用取消接口

--- src/main/java/com/xescm/ofc/service/impl/OfcOrderManageServiceImpl.java
+++ src/main/java/com/xescm/ofc/service/impl/OfcOrderManageServiceImpl.java
@@ -894,18 +894,13 @@ public class OfcOrderManageServiceImpl  implements OfcOrderManageService {
                     dto.setWarehouseID(ofcSiloprogramInfo.getWarehouseCode());
                     dto.setReason("");
                     logger.info("==> 仓储计划单号{}开始取消,业务类型为{}",ofcSiloprogramInfo.getPlanCode(),ofcSiloprogramInfo.getDocumentType());
-                        //调用接口尝试3次
-                        for (int j = 0; j <3; j++) {
-                            response=whcOrderCancelEdasService.cancelOrder(dto);
-                            if(response!=null){
-                                break;
-                            }
-                        }
+                     response=whcOrderCancelEdasService.cancelOrder(dto);
                 } catch (Exception e) {
                     logger.error("仓储计划单调用WHC取消端口出现异常{}",e.getMessage(),e);
-                    e.printStackTrace();
                     throw new BusinessException(e.getMessage(),e);
-
+                }
+                if(response==null){
+                    throw new BusinessException("仓储计划单调用WHC取消端口出现异常");
                 }
                 if(Response.ERROR_CODE == response.getCode()){
                     logger.error("仓储计划单调用WHC响应状态码{}",response.getCode());
