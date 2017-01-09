ffb563c7cb86409a3b00b6cc21391e40e750d4bf
罗迎豪 <lyhluo@163.com>
2017-01-09 14:08:19 +0800

    基地名称展示

--- src/main/java/com/xescm/ofc/domain/OrderSearchOperResult.java
+++ src/main/java/com/xescm/ofc/domain/OrderSearchOperResult.java
@@ -2,10 +2,12 @@ package com.xescm.ofc.domain;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
+@Data
 public class OrderSearchOperResult {
 
     private String custName;
@@ -24,6 +26,8 @@ public class OrderSearchOperResult {
 
     private String businessType;
 
+    private String baseName;
+
     private String orderStatus;
 
     private String consigneeType;
@@ -38,116 +42,6 @@ public class OrderSearchOperResult {
 
     private String storeName;
 
-    public String getCustName() {
-        return custName;
-    }
 
-    public void setCustName(String custName) {
-        this.custName = custName;
-    }
-
-    public String getOrderCode() {
-        return orderCode;
-    }
-
-    public void setOrderCode(String orderCode) {
-        this.orderCode = orderCode;
-    }
-
-    public String getCustOrderCode() {
-        return custOrderCode;
-    }
-
-    public void setCustOrderCode(String custOrderCode) {
-        this.custOrderCode = custOrderCode;
-    }
-
-    public String getOrderBatchNumber() {
-        return orderBatchNumber;
-    }
-
-    public void setOrderBatchNumber(String orderBatchNumber) {
-        this.orderBatchNumber = orderBatchNumber;
-    }
-
-    public Date getOrderTime() {
-        return orderTime;
-    }
-
-    public void setOrderTime(Date orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    public String getOrderType() {
-        return orderType;
-    }
-
-    public void setOrderType(String orderType) {
-        this.orderType = orderType;
-    }
-
-    public String getBusinessType() {
-        return businessType;
-    }
-
-    public void setBusinessType(String businessType) {
-        this.businessType = businessType;
-    }
-
-    public String getOrderStatus() {
-        return orderStatus;
-    }
-
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public String getConsigneeType() {
-        return consigneeType;
-    }
-
-    public void setConsigneeType(String consigneeType) {
-        this.consigneeType = consigneeType;
-    }
-
-    public String getConsigneeName() {
-        return consigneeName;
-    }
-
-    public void setConsigneeName(String consigneeName) {
-        this.consigneeName = consigneeName;
-    }
-
-    public String getConsigneeContactName() {
-        return consigneeContactName;
-    }
-
-    public void setConsigneeContactName(String consigneeContactName) {
-        this.consigneeContactName = consigneeContactName;
-    }
-
-    public String getWarehouseName() {
-        return warehouseName;
-    }
-
-    public void setWarehouseName(String warehouseName) {
-        this.warehouseName = warehouseName;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
 
 }
