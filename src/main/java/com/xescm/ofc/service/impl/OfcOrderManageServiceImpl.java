e26fa11bfd6ca7e5488146e9b2d7c66f678fbdfd
罗迎豪 <lyhluo@163.com>
2017-02-10 10:17:11 +0800

    创单卡班类型不审核BUG

--- src/main/java/com/xescm/ofc/service/impl/OfcOrderManageServiceImpl.java
+++ src/main/java/com/xescm/ofc/service/impl/OfcOrderManageServiceImpl.java
@@ -1784,9 +1784,11 @@ public class OfcOrderManageServiceImpl  implements OfcOrderManageService {
                     //运输订单
                     OfcTransplanInfo ofcTransplanInfo=new OfcTransplanInfo();
                     ofcTransplanInfo.setProgramSerialNumber("1");
-                    //if (!PubUtils.trimAndNullAsEmpty(ofcFundamentalInformation.getBusinessType()).equals(WITHTHEKABAN)){//在城配下单这边没有卡班
+                    if (!PubUtils.trimAndNullAsEmpty(ofcFundamentalInformation.getBusinessType()).equals(WITHTHEKABAN)){//在城配下单这边没有卡班
                         transPlanCreate(ofcTransplanInfo,ofcFundamentalInformation,goodsDetailsList,ofcDistributionBasicInfo,ofcFundamentalInformation.getCustName(),ofcFinanceInformation);
-                    //}
+                    }else{
+                        transPlanCreateKaBan(ofcTransplanInfo,ofcFundamentalInformation,goodsDetailsList,ofcDistributionBasicInfo,ofcFinanceInformation,userName);
+                    }
                 }else if(PubUtils.trimAndNullAsEmpty(ofcFundamentalInformation.getOrderType()).equals(WAREHOUSEDISTRIBUTIONORDER)
                         && PubUtils.trimAndNullAsEmpty(ofcFundamentalInformation.getBusinessType()).equals(SALESOUTOFTHELIBRARY)
                         && ofcWarehouseInformation.getProvideTransport() == WAREHOUSEORDERPROVIDETRANS){            // 仓配订单
