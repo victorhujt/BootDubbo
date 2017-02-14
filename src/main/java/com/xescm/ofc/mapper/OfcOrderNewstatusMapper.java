b73ef4520448ad61e6df7fcb50b71443db0fa96a
yangdongxu <1037735036@qq.com>
2017-02-14 14:41:25 +0800

    订单状态

---
+++ src/main/java/com/xescm/ofc/mapper/OfcOrderNewstatusMapper.java
@@ -0,0 +1,7 @@
+package com.xescm.ofc.mapper;
+
+import com.xescm.ofc.domain.OfcOrderNewstatus;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface OfcOrderNewstatusMapper extends Mapper<OfcOrderNewstatus> {
+}
---
+++ src/main/java/com/xescm/ofc/service/impl/OfcOrderNewstatusServiceImpl.java
@@ -0,0 +1,11 @@
+package com.xescm.ofc.service.impl;
+
+import com.xescm.ofc.domain.OfcOrderNewstatus;
+import com.xescm.ofc.service.OfcOrderNewstatusService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OfcOrderNewstatusServiceImpl extends BaseService<OfcOrderNewstatus> implements OfcOrderNewstatusService {
+}
