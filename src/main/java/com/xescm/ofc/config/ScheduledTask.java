2788ee896d1a2f8620fded547473082de4a7027a
yangdongxu <1037735036@qq.com>
2017-03-01 10:56:52 +0800

    Merge branch 'dev_edas' of devtool.xianyiscm.com:cus/xescm-ofc into dev_edas
    # Conflicts:
    #	src/main/java/com/xescm/ofc/config/ScheduledTask.java

--- src/main/java/com/xescm/ofc/config/ScheduledTask.java
+++ src/main/java/com/xescm/ofc/config/ScheduledTask.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
--- src/main/java/com/xescm/ofc/edas/service/impl/OfcOrderStatusEdasServiceImpl.java
+++ src/main/java/com/xescm/ofc/edas/service/impl/OfcOrderStatusEdasServiceImpl.java
@@ -11,7 +11,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
