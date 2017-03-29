d4843823a3b40095ae86b9eb1d38b389d81a81df
罗迎豪 <lyhluo@163.com>
2017-03-29 20:34:23 +0800

    订单日期校验失败

--- src/main/java/com/xescm/ofc/utils/ExcelCheckUtils.java
+++ src/main/java/com/xescm/ofc/utils/ExcelCheckUtils.java
@@ -13,6 +13,6 @@ public class ExcelCheckUtils {
     //校验日期格式是否为: yyyy-MM-dd HH:mm:ss 或 yyyy-MM-dd
    public static boolean checkDateTimeFormat(String dateTime){
        String[] split = dateTime.split(" ");
-       return split.length > 1 ? split[0].matches(REGEX_YYYYMMDD) && split[1].matches(PubUtils.REGX_TIME) : dateTime.matches(REGEX_YYYYMMDD);
+       return split.length > 1 ? split[0].matches(REGEX_YYYYMMDD) || split[1].matches(PubUtils.REGX_TIME) : dateTime.matches(REGEX_YYYYMMDD);
    }
 }
