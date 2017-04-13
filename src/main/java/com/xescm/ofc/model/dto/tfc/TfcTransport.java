d3d3d63dac8c7e7447630b7dbb35f2439b2669f4
罗迎豪 <lyhluo@163.com>
2017-04-13 20:03:47 +0800

    推送TFC追加字段: 订单明细追加pickupTime字段

--- src/main/java/com/xescm/ofc/model/dto/tfc/TfcTransport.java
+++ src/main/java/com/xescm/ofc/model/dto/tfc/TfcTransport.java
@@ -196,7 +196,6 @@ public class TfcTransport implements Serializable {
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    @Column(name = "pickup_time")
     private Date pickupTime;
 
 
