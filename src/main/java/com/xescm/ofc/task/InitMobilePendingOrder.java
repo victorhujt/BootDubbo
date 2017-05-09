99fc9d746411dddb35c4b36d49c96d03a471af13
胡金涛 <victorhjt@163.com>
2017-05-09 14:45:00 +0800

    增加排查的日志

--- src/main/java/com/xescm/ofc/task/InitMobilePendingOrder.java
+++ src/main/java/com/xescm/ofc/task/InitMobilePendingOrder.java
@@ -3,6 +3,9 @@ package com.xescm.ofc.task;
 import com.xescm.ofc.domain.OfcMobileOrder;
 import com.xescm.ofc.model.dto.form.MobileOrderOperForm;
 import com.xescm.ofc.service.OfcMobileOrderService;
+import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +13,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 import static com.xescm.ofc.constant.OrderConstConstant.MOBILE_PENDING_ORDER_LIST;
+import static com.xescm.ofc.constant.OrderConstConstant.UN_TREATED;
 
 /**
  * @description: ${TODO}
@@ -19,19 +23,27 @@ import static com.xescm.ofc.constant.OrderConstConstant.MOBILE_PENDING_ORDER_LIS
 @Component
 public class InitMobilePendingOrder implements CommandLineRunner {
 
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
     private OfcMobileOrderService ofcMobileOrderService;
 
     @Override
     public void run(String... strings) throws Exception {
-
+        logger.info("==>手机订单号同步到redis缓存开始执行");
         MobileOrderOperForm params = new MobileOrderOperForm();
-        params.setMobileOrderStatus("0");
+        params.setMobileOrderStatus(UN_TREATED);
         List<OfcMobileOrder> mobileOrders = ofcMobileOrderService.queryOrderList(params);
-
-        for (int i = 0 ; i < mobileOrders.size(); i++) {
-            OfcMobileOrder order = mobileOrders.get(i);
-            ofcMobileOrderService.pushOrderToCache(MOBILE_PENDING_ORDER_LIST,order.getMobileOrderCode());
+        if(!CollectionUtils.isEmpty(mobileOrders)){
+            logger.info("==>查询到未受理的订单总数为{}",mobileOrders.size());
+            for (int i = 0 ; i < mobileOrders.size(); i++) {
+                OfcMobileOrder order = mobileOrders.get(i);
+                ofcMobileOrderService.pushOrderToCache(MOBILE_PENDING_ORDER_LIST,order.getMobileOrderCode());
+            }
+        }else{
+            logger.info("==>未查询到未受理的订单");
         }
+
+
+
     }
 }
