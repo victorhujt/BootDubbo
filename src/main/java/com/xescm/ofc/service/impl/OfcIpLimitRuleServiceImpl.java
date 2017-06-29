270314b763b4b36f264137491f302b99b195517a
韩光芳 <hgh.han@gmail.com>
2017-06-29 15:36:49 +0800

    修改订单任务日志、接收日志查询页码bug

--- src/main/java/com/xescm/ofc/service/impl/OfcIpLimitRuleServiceImpl.java
+++ src/main/java/com/xescm/ofc/service/impl/OfcIpLimitRuleServiceImpl.java
@@ -31,65 +31,69 @@ public class OfcIpLimitRuleServiceImpl extends BaseService<OfcIplimitRule> imple
     private IpLimitRuleConfig ipLimitRuleConfig;
     @Resource
     private RedisOperationUtils redisOperationUtils;
+
     @Override
     public void updateByIp(OfcIplimitRule rule) {
         ofcIplimitRuleMapper.updateByIp(rule);
     }
 
     @Override
-    public void  checkLimit(RedisOperationUtils redisOperationUtils, HttpServletRequest request) {
+    public void checkLimit(RedisOperationUtils redisOperationUtils, HttpServletRequest request) {
         boolean isSend = false;
         String ip = IpUtils.getIpAddr(request);
-        if(redisOperationUtils.hasKey("SMS:"+ip)){
+        if (redisOperationUtils.hasKey("SMS:" + ip)) {
             isSend = true;
         }
         //验证ip是否冻结 缓存redis 10分钟
-        if(redisOperationUtils.hasKey("ip:"+ip)){
-            redisOperationUtils.increment("ip:"+ip+":"+QUERY_REQUEST_COUNT, 1L);
-            CheckUtils.checkArgument(redisOperationUtils.hasKey("ip:"+ip), ResultCodeEnum.IPISFREEZE);
+        if (redisOperationUtils.hasKey("ip:" + ip)) {
+            redisOperationUtils.increment("ip:" + ip + ":" + QUERY_REQUEST_COUNT, 1L);
+            CheckUtils.checkArgument(redisOperationUtils.hasKey("ip:" + ip), ResultCodeEnum.IPISFREEZE);
         }
         //验证ip是否已经被加入黑名单
         OfcIplimitRule ofcIpLimitRule = new OfcIplimitRule();
         ofcIpLimitRule.setIp(ip);
         List<OfcIplimitRule> rules = select(ofcIpLimitRule);
-        if(!CollectionUtils.isEmpty(rules)){
+        if (!CollectionUtils.isEmpty(rules)) {
             OfcIplimitRule ofcIplimitRule = rules.get(0);
             CheckUtils.checkArgument(IS_BLACK.equals(ofcIplimitRule.getBlack()), ResultCodeEnum.IPISFORBIDDEN);
         }
         Long currentTime = System.currentTimeMillis();
-        if(redisOperationUtils.hasKey("ip:"+ip+":"+QUERY_REQUEST_COUNT)){
-            Long reqCount =  redisOperationUtils.getValue("ip:"+ip+":"+QUERY_REQUEST_COUNT);
-            Long first = redisOperationUtils.getValue("ip:"+ip+":"+FIRST_REQUEST_TIME);
-            redisOperationUtils.increment("ip:"+ip+":"+QUERY_REQUEST_COUNT, 1L);
+        if (redisOperationUtils.hasKey("ip:" + ip + ":" + QUERY_REQUEST_COUNT)) {
+            Long reqCount = redisOperationUtils.getValue("ip:" + ip + ":" + QUERY_REQUEST_COUNT);
+            Long first = redisOperationUtils.getValue("ip:" + ip + ":" + FIRST_REQUEST_TIME);
+            redisOperationUtils.increment("ip:" + ip + ":" + QUERY_REQUEST_COUNT, 1L);
 
-            logger.info("距离第一次请求的时间为:{}",currentTime - first+"ms");
-            logger.info("请求的次数为:{}",reqCount);
+            logger.info("距离第一次请求的时间为:{}", currentTime - first + "ms");
+            logger.info("请求的次数为:{}", reqCount);
+
+            // 距第一次调用时长
+            long timeDiff = currentTime - first;
             //第一阀值
-            if(!isSend){
-                if((currentTime - first) > ipLimitRuleConfig.getFristMinTime()*60*1000 && (currentTime - first) < ipLimitRuleConfig.getFristMaxTime()*60*1000){
+            if (!isSend) {
+                if (timeDiff > ipLimitRuleConfig.getFristMinTime() * 60 * 1000 && timeDiff < ipLimitRuleConfig.getFristMaxTime() * 60 * 1000) {
                     CheckUtils.checkArgument(reqCount > ipLimitRuleConfig.getFirstThresholdMin() && reqCount < ipLimitRuleConfig.getFirstThresholdMax(), ResultCodeEnum.OPERATIONSTOOFREQUENT);
                 }
             }
             //第二阀值
-            if((currentTime - first) > ipLimitRuleConfig.getSecondMinTime()*60*1000 && (currentTime - first)< ipLimitRuleConfig.getSecondMaxTime()*60*1000){
-                if(reqCount > ipLimitRuleConfig.getSecondThresholdMin() && reqCount < ipLimitRuleConfig.getSecondThresholdMax()){
-                    redisOperationUtils.set("ip:"+ip,"freezing",5L, TimeUnit.MINUTES);//ip缓存五分钟
-                    logger.info("ip缓存5分钟，{}",ip);
+            if (timeDiff > ipLimitRuleConfig.getSecondMinTime() * 60 * 1000 && timeDiff < ipLimitRuleConfig.getSecondMaxTime() * 60 * 1000) {
+                if (reqCount > ipLimitRuleConfig.getSecondThresholdMin() && reqCount < ipLimitRuleConfig.getSecondThresholdMax()) {
+                    redisOperationUtils.set("ip:" + ip, "freezing", 5L, TimeUnit.MINUTES);//ip缓存五分钟
+                    logger.info("ip缓存5分钟，{}", ip);
                 }
             }
             //第三阀值
-            if(currentTime - first > ipLimitRuleConfig.getThirdMinTime()*60*1000){
-                if(reqCount > ipLimitRuleConfig.getThirdThresholdMin()) {
+            if (timeDiff > ipLimitRuleConfig.getThirdMinTime() * 60 * 1000) {
+                if (reqCount > ipLimitRuleConfig.getThirdThresholdMin()) {
                     OfcIplimitRule rule = new OfcIplimitRule();
                     rule.setBlack(IS_BLACK);
                     rule.setIp(ip);
                     save(rule);
-                    logger.info("ip加入黑名单，{}",ip);
+                    logger.info("ip加入黑名单，{}", ip);
                 }
             }
-        }else{
-            redisOperationUtils.set("ip:"+ip+":"+QUERY_REQUEST_COUNT,String.valueOf(1L),15L,TimeUnit.MINUTES);
-            redisOperationUtils.set("ip:"+ip+":"+FIRST_REQUEST_TIME,String.valueOf(System.currentTimeMillis()),15L,TimeUnit.MINUTES);
+        } else {
+            redisOperationUtils.set("ip:" + ip + ":" + QUERY_REQUEST_COUNT, String.valueOf(1L), 15L, TimeUnit.MINUTES);
+            redisOperationUtils.set("ip:" + ip + ":" + FIRST_REQUEST_TIME, String.valueOf(System.currentTimeMillis()), 15L, TimeUnit.MINUTES);
         }
     }
 }
