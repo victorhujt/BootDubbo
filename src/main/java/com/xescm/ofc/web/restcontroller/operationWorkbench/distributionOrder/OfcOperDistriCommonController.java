a1d64b460266f0527b79d52ca4729169709608cd
罗迎豪 <lyhluo@163.com>
2017-07-05 15:17:30 +0800

    新目录

---
+++ src/main/java/com/xescm/ofc/web/restcontroller/operationWorkbench/distributionOrder/OfcOperDistriCommonController.java
@@ -0,0 +1,11 @@
+package com.xescm.ofc.web.restcontroller.operationWorkbench.distributionOrder;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * Created by lyh on 2017/7/5.
+ */
+@Controller
+public class OfcOperDistriCommonController {
+}
