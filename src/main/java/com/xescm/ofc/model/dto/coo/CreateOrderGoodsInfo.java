656ab04ed6dd88305c8de904c22753b698775bc0
胡金涛 <victorhjt@163.com>
2017-09-08 11:07:03 +0800

    注解代替get和set方法

--- src/main/java/com/xescm/ofc/model/dto/coo/CreateOrderGoodsInfo.java
+++ src/main/java/com/xescm/ofc/model/dto/coo/CreateOrderGoodsInfo.java
@@ -2,6 +2,7 @@ package com.xescm.ofc.model.dto.coo;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.xescm.csc.model.dto.packing.GoodsPackingDto;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -13,27 +14,12 @@ import java.util.List;
  * Created by hiyond on 2016/11/14.
  */
 @XStreamAlias("orderDetail")
+@Data
 public class CreateOrderGoodsInfo implements Serializable {
 
     private static final long serialVersionUID = 7909253575176134994L;
     /*客户名称*/
     private String custName;
-
-    public String getCustName() {
-        return custName;
-    }
-
-    public void setCustName(String custName) {
-        this.custName = custName;
-    }
-
-    public String getCustCode() {
-        return custCode;
-    }
-
-    public void setCustCode(String custCode) {
-        this.custCode = custCode;
-    }
 
     /**客户编码*/
 
@@ -58,22 +44,6 @@ public class CreateOrderGoodsInfo implements Serializable {
      * 货品大类
      */
     private String goodsType;
-
-    public String getGoodsTypeCode() {
-        return goodsTypeCode;
-    }
-
-    public void setGoodsTypeCode(String goodsTypeCode) {
-        this.goodsTypeCode = goodsTypeCode;
-    }
-
-    public String getGoodsCategoryCode() {
-        return goodsCategoryCode;
-    }
-
-    public void setGoodsCategoryCode(String goodsCategoryCode) {
-        this.goodsCategoryCode = goodsCategoryCode;
-    }
 
     /**
      * 货品大类编码
@@ -160,23 +130,7 @@ public class CreateOrderGoodsInfo implements Serializable {
     /**
      * 平台行号
      */
-    private Integer passLineNo;
-
-    public Integer getPassLineNo() {
-        return passLineNo;
-    }
-
-    public void setPassLineNo(Integer passLineNo) {
-        this.passLineNo = passLineNo;
-    }
-
-    public String getExternalPaasLineNo() {
-        return externalPaasLineNo;
-    }
-
-    public void setExternalPaasLineNo(String externalPaasLineNo) {
-        this.externalPaasLineNo = externalPaasLineNo;
-    }
+    private Long passLineNo;
 
     /**
      * 外部平台号
@@ -189,176 +143,4 @@ public class CreateOrderGoodsInfo implements Serializable {
     private String manufactureBatchLotatt;
 
     private List<GoodsPackingDto> skuPackageList;
-
-    public List<GoodsPackingDto> getSkuPackageList() {
-        return skuPackageList;
-    }
-
-    public void setSkuPackageList(List<GoodsPackingDto> skuPackageList) {
-        this.skuPackageList = skuPackageList;
-    }
-
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
-
-    public String getSupportBatch() {
-        return supportBatch;
-    }
-
-    public void setSupportBatch(String supportBatch) {
-        this.supportBatch = supportBatch;
-    }
-
-    public String getSupportName() {
-        return supportName;
-    }
-
-    public void setSupportName(String supportName) {
-        this.supportName = supportName;
-    }
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
-    public String getPackageType() {
-        return packageType;
-    }
-
-    public void setPackageType(String packageType) {
-        this.packageType = packageType;
-    }
-
-    public BigDecimal getConversionRate() {
-        return conversionRate;
-    }
-
-    public void setConversionRate(BigDecimal conversionRate) {
-        this.conversionRate = conversionRate;
-    }
-
-    public BigDecimal getPrimaryQuantity() {
-        return primaryQuantity;
-    }
-
-    public void setPrimaryQuantity(BigDecimal primaryQuantity) {
-        this.primaryQuantity = primaryQuantity;
-    }
-
-    public String getManufactureBatchLotatt() {
-        return manufactureBatchLotatt;
-    }
-
-    public void setManufactureBatchLotatt(String manufactureBatchLotatt) {
-        this.manufactureBatchLotatt = manufactureBatchLotatt;
-    }
-
-    public String getGoodsCode() {
-        return goodsCode;
-    }
-
-    public void setGoodsCode(String goodsCode) {
-        this.goodsCode = goodsCode;
-    }
-
-    public String getGoodsName() {
-        return goodsName;
-    }
-
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
-
-    public String getGoodsSpec() {
-        return goodsSpec;
-    }
-
-    public void setGoodsSpec(String goodsSpec) {
-        this.goodsSpec = goodsSpec;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
-
-    public String getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(String quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getUnitPrice() {
-        return unitPrice;
-    }
-
-    public void setUnitPrice(String unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public String getProductionBatch() {
-        return productionBatch;
-    }
-
-    public void setProductionBatch(String productionBatch) {
-        this.productionBatch = productionBatch;
-    }
-
-    public String getProductionTime() {
-        return productionTime;
-    }
-
-    public void setProductionTime(String productionTime) {
-        this.productionTime = productionTime;
-    }
-
-    public String getInvalidTime() {
-        return invalidTime;
-    }
-
-    public void setInvalidTime(String invalidTime) {
-        this.invalidTime = invalidTime;
-    }
-
-    public String getWeight() {
-        return weight;
-    }
-
-    public String getCubage() {
-        return cubage;
-    }
-
-    public void setWeight(String weight) {
-        this.weight = weight;
-    }
-
-    public void setCubage(String cubage) {
-        this.cubage = cubage;
-    }
-
-    public String getGoodsType() {
-        return goodsType;
-    }
-
-    public void setGoodsType(String goodsType) {
-        this.goodsType = goodsType;
-    }
-
-    public String getGoodsCategory() {
-        return goodsCategory;
-    }
-
-    public void setGoodsCategory(String goodsCategory) {
-        this.goodsCategory = goodsCategory;
-    }
 }
