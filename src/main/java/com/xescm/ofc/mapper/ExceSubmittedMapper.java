ba2602703a8f096a9d34f15d5fb33a9c5d8eab38
袁宝龙 <dragon@dragon-yuan.me>
2017-11-06 18:43:06 +0800

    [+,*] 异常报送初始化项目；

---
+++ src/main/java/com/xescm/ofc/mapper/ExceSubmittedMapper.java
@@ -0,0 +1,9 @@
+package com.xescm.ofc.mapper;
+
+import com.xescm.ofc.domain.ExceSubmitted;
+import com.xescm.ofc.utils.MyMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExceSubmittedMapper extends MyMapper<ExceSubmitted> {
+}
