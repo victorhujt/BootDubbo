6fb4496ae84537fbab25d8d4549f5a470f61226d
胡金涛 <victorhjt@163.com>
2017-11-20 11:45:10 +0800

    数量反馈转换为实际包装的单位

--- src/main/java/com/xescm/ofc/mapper/OfcOrderNewstatusMapper.java
+++ src/main/java/com/xescm/ofc/mapper/OfcOrderNewstatusMapper.java
@@ -1,10 +1,11 @@
 package com.xescm.ofc.mapper;
 
 import com.xescm.ofc.domain.OfcOrderNewstatus;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.Map;
-
+@Repository
 public interface OfcOrderNewstatusMapper extends Mapper<OfcOrderNewstatus> {
     OfcOrderNewstatus orderStatusSelectNew(Map<String, String> mapperMap);
 }
